4831e3b2ba3cc1045463d3f7ff76a0353a8f6844
uma sharma <umasharmabtech@gmail.com>
2026-05-07 22:14:19 +0530

    feat: handle multiple numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,9 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
+  //   if (numbers.includes(",")) {
+  //     return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
+  //   }
   if (numbers.includes(",")) {
     return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
   }
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -11,3 +11,6 @@ test("returns number when single number is passed", () => {
 test("returns sum of two comma-separated numbers", () => {
   expect(add("1,2")).toBe(3);
 });
+test("returns sum of multiple comma-separated numbers", () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
