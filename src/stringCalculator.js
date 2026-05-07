974a73d0fb2ddd99e1e0162aca701e2f6bcc5193
uma sharma <umasharmabtech@gmail.com>
2026-05-07 21:52:11 +0530

    feat: handle single number input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,11 @@
+// function add(numbers) {
+//   if (numbers === "") return 0;
+// }
+
+// module.exports = add;
 function add(numbers) {
   if (numbers === "") return 0;
+  return parseInt(numbers);
 }
 
 module.exports = add;
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,5 +1,14 @@
+// const add = require("../src/stringCalculator");
+
+// test("returns 0 for empty string", () => {
+//   expect(add("")).toBe(0);
+// });
 const add = require("../src/stringCalculator");
 
 test("returns 0 for empty string", () => {
   expect(add("")).toBe(0);
+});
+
+test("returns number when single number is passed", () => {
+  expect(add("1")).toBe(1);
 });
